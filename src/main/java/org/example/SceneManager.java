0871954cbb3ac9c96377f0487d0061575f508f6c
oussema hammadi <ohammadi.oh@gmail.com>
2024-09-24 14:38:23 +0200

    first commit

---
+++ src/main/java/org/example/SceneManager.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneManager {
+
+    private static SceneManager instance;
+    private Stage stage;
+
+    private SceneManager() {
+    }
+
+    public static synchronized SceneManager getInstance() {
+        if (instance == null) {
+            instance = new SceneManager();
+        }
+        return instance;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void loadScene(String fxmlFile) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Parent root = fxmlLoader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
