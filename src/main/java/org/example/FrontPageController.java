6c460dd296c6523b656e382e3628b1194f17f32c
oussema hammadi <ohammadi.oh@gmail.com>
2024-09-30 12:20:13 +0200

    Adding game explanation

--- src/main/java/org/example/FrontPageController.java
+++ src/main/java/org/example/FrontPageController.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.io.IOException;
 
@@ -13,8 +15,10 @@ public class FrontPageController {
     @FXML
     private Button highScoresButton;
 
+    @FXML
+    private Label welcomeLabel;
 
-
+    // Initialize method called when the controller is loaded
     @FXML
     public void initialize() {
         try {
@@ -24,8 +28,23 @@ public class FrontPageController {
         }
     }
 
+    // This method is linked to the "Start" button in your FXML
     @FXML
-    public void startGame() {
+    private void startGame() {
+        displayWelcomeMessage();
+        loadGameScene();
+    }
+
+    private void displayWelcomeMessage() {
+        // Create an alert dialog for the welcome message
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Welcome to the Game!");
+        alert.setHeaderText("Welcome to the Game!");
+        alert.setContentText("Navigate the board by moving the stone. Avoid framed cells and try to reach the goal!");
+        alert.showAndWait();
+    }
+
+    private void loadGameScene() {
         try {
             SceneManager.getInstance().loadScene("/board.fxml");
         } catch (IOException e) {
@@ -36,12 +55,10 @@ public class FrontPageController {
     @FXML
     public void viewHighScores() {
         try {
-            initialize();
+            initialize(); // This might not be necessary here, depending on how you manage high scores
             SceneManager.getInstance().loadScene("/high_scores.fxml");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-
 }
